de2c402cc046ec2e35d821dfc62e479bb3cdc605
soumyas567 <14910227+soumyas567@users.noreply.github.com>
2021-12-28 10:09:23 +0000

    data inserted

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
 import mysql.connector as connector
-connector = connector.connect(host="localhost", user="root", passwd="Minimum$15")
+connector = connector.connect(host="eu-cdbr-west-02.cleardb.net", user="b654141abcadfd", passwd="9b1b1f68")
 cursor = connector.cursor()
-query = "CREATE DATABASE technews"
+query = "CREATE DATABASE heroku_75ce60fabbd29e1"
 cursor.execute(query)
 cursor.execute("SHOW DATABASES")
 for db in cursor:
--- tnaggregator/__init__.py
+++ tnaggregator/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-DB_NAME  = 'technews' 
-password = 'Minimum$15'
+DB_NAME  = 'heroku_75ce60fabbd29e1' 
+password = '9b1b1f68'
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{password}@localhost/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://b654141abcadfd:{password}@eu-cdbr-west-02.cleardb.net/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db.init_app(app)
 from .views import views
